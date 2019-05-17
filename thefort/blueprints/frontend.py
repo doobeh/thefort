72e74f6f2de067ff211fab1fcc0dcc8e62594ad7
Anthony Plunkett <anthony@thefort.org>
2019-05-16 22:45:12 -0400

    Article initial implementation.

--- thefort/blueprints/frontend.py
+++ thefort/blueprints/frontend.py
@@ -1,4 +1,6 @@
-from flask import current_app, Blueprint, render_template
+from flask import Blueprint, render_template
+from thefort.models import Article, Tag
+
 
 bp = Blueprint('frontend', __name__)
 
@@ -6,3 +8,15 @@ bp = Blueprint('frontend', __name__)
 @bp.route('/')
 def home():
     return render_template('index.html')
+
+
+@bp.route('/a/<slug>')
+def article(slug):
+    a = Article.query.filter_by(slug=slug).first_or_404()
+    return render_template('article.html', article=a)
+
+
+@bp.route('/t/<tag>')
+def tag(tag):
+    t = Tag.query.filter_by(name=tag).first_or_404()
+    return render_template('tag.html', tag=t)
