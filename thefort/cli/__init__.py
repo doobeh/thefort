7687b96037170b757bc93b54949fbbdd0d2e513c
Anthony Plunkett <anthony@thefort.org>
2019-06-12 16:34:24 -0400

    Clean up CLI

--- thefort/cli/__init__.py
+++ thefort/cli/__init__.py
@@ -5,6 +5,7 @@ from flask import current_app
 import os
 import random
 from datetime import datetime
+from thefort.models import User, Role
 
 core_cli = AppGroup("core")
 user_cli = AppGroup("user")
@@ -24,7 +25,7 @@ def app_init():
         print("[x] Created instance folder")
     db.create_all()
     print("[x] Created database")
-    roles = ["admin", "user", "data"]
+    roles = ["admin", "user"]
     for role in roles:
         r = Role(role)
         db.session.add(r)
@@ -75,80 +76,3 @@ def assign_user(username, role):
 def user_list_all():
     users = [f"    * {x!r}" for x in User.query.all()]
     return print("\n".join(users))
-
-
-@core_cli.command("import_products")
-@click.option("--filename", prompt=True, default="item_dump.csv")
-def load_product(filename):
-    fn = os.path.join(current_app.instance_path, "data", filename)
-    total_counter, update_counter, insert_counter = import_items(fn)
-    return total_counter, update_counter, insert_counter
-
-
-@core_cli.command("random_category")
-def random_category():
-    c = [x for x in Category.query.all()]
-    p = Product.query.filter_by(active=True).all()
-    for product in p:
-        product.categories.append(random.choice(c))
-    db.session.commit()
-
-
-@core_cli.command("update_categories")
-@click.option("--filename", prompt=True, default="data_export.json")
-def update_categories(filename):
-    """ Updates existing products with categories provided from a .json formatted file
-
-        file formatted as [{'gtin': '000000000000000', categories=['aaaa', 'bbbb'], ...]
-        The categories are the `slug` reference to keep things easier for matching.
-    """
-    fn = os.path.join(current_app.instance_path, "data", filename)
-    update_category_to_products(fn)
-    click.echo("Categories mapped.")
-
-
-@core_cli.command("update_descriptions")
-@click.option("--filename", prompt=True, default="description_map.json")
-def map_categories(filename):
-    fn = os.path.join(current_app.instance_path, "data", filename)
-    map_description_to_products(fn)
-    click.echo("Descriptions updated.")
-
-
-@core_cli.command("activate_product")
-def activate_product():
-    db.session.query(Product).update({"active": True})
-    db.session.commit()
-
-
-@core_cli.command("random_unit")
-def random_unit():
-    c = ["Full Case", "Half Case", "Single"]
-    p = Product.query.filter_by(active=True).all()
-    for product in p:
-        product.unit = random.choice(c)
-    db.session.commit()
-
-
-@core_cli.command("guessed_unit")
-def guessed_unit():
-    p = Product.query.all()
-
-    for product in p:
-        ids = f"{product.id}"
-        if len(ids) == 5:
-            print(f"Len is 5 for {product}")
-            # click.echo('Length Match of 5')
-            if ids[0] == "7":
-                # click.echo('Updating Each')
-                click.echo(f"setting {product} as EACH")
-                product.unit = "EACH"
-            elif ids[0] == "6":
-                # click.echo('Updating Half-Case')
-                product.unit = "HALF-CASE"
-        else:
-            if ids[-1] == "5":
-                product.unit = "EACH"
-
-    db.session.commit()
-    click.echo("Guessed Units!")
